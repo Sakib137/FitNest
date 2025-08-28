f137a5731a6742a82dfc7fcaa70dd8c1f84ad70d
Sakib137 <mdsakib124202@gmail.com>
2025-08-28 22:50:26 +0600

    Initial Commit

---
+++ FitNest/FitNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitNestApp.swift
+//  FitNest
+//
+//  Created by Md Sakib Al Hasan on 28/8/25.
+//
+
+import SwiftUI
+
+@main
+struct FitNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitNestTests/FitNestTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitNestTests.swift
+//  FitNestTests
+//
+//  Created by Md Sakib Al Hasan on 28/8/25.
+//
+
+import Testing
+@testable import FitNest
+
+struct FitNestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
